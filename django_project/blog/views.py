3da0477d4c4ea9ea3c08a6c4cdd05a07df33ad47
alihaghayegh <haghayeghali95@gmail.com>
2024-02-18 14:59:44 +0330

    post form and post create view added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView)
 from .models import Post
 
 
@@ -20,6 +22,11 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 
 def about(request):
